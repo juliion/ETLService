b506540612265c1fe7595648d58437b8630429f1
juliion <juliaionova111@gmail.com>
2022-08-03 17:51:05 +0300

    Add basic services.
    EtlService is responsible for managing the entire process. FileService - helper for working with files and folders I / O.

---
+++ ETLService/EtlServices/EtlService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using ETLService.Extractors.Readers;
+using ETLService.Transformers;
+using ETLService.Loaders.Writers;
+using ETLService.Models;
+using ETLService.Converters;
+
+namespace ETLService.EtlServices
+{
+    public class EtlService
+    {
+        private FileService _fileService;
+        object locker = new object();
+        public EtlService(FileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        public void Run()
+        {
+            _fileService.CreateSubfolder();
+            string[] inputFiles = _fileService.GetFilesFromInputFolder();
+            inputFiles.AsParallel().ForAll((fname) => ProcessFile(fname));
+        }
+
+        private void ProcessFile(string filename)
+        {
+            lock(locker)
+            {
+                FileReader reader = new TxtReader(filename, new TransactionConverter());
+                TransactionTransformer transformer = new TransactionTransformer();
+                FileWriter writer = new JSONWriter(_fileService.CreateFileInCurrSubfolder());
+                List<Transaction> transactions = reader.Extract();
+                List<TransactionsByCity> transformedTransactions = transformer.Transform(transactions);
+                writer.Load(transformedTransactions);
+            }
+        }
+    }
+}
---
+++ ETLService/EtlServices/FileService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETLService.EtlServices
+{
+    public class FileService
+    {
+        private readonly string _outputPath;
+        private readonly string _inputPath;
+        private string _currentSubfolder;
+        private int _currentFileNumber;
+        public FileService(string outputPath, string inputPath)
+        {
+            _outputPath = outputPath;
+            _inputPath = inputPath;
+            _currentFileNumber = 0;
+        }
+
+        public string[] GetFilesFromInputFolder()
+        {
+            return Directory.GetFiles(_inputPath);
+        }
+        public void CreateSubfolder()
+        {
+            _currentSubfolder = Path.Combine(_outputPath, DateTime.Today.ToShortDateString());
+            Directory.CreateDirectory(_currentSubfolder);
+        }
+        public string CreateFileInCurrSubfolder()
+        {
+            _currentFileNumber++;
+            string filePath = Path.Combine(_currentSubfolder, $"output{_currentFileNumber}.json");
+            using var newFile = File.Create(filePath);
+            return filePath;
+        }
+    }
+}
