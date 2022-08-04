7b09269059ea9a29b0c1bc86b835460eabe51585
juliion <juliaionova111@gmail.com>
2022-08-04 16:53:04 +0300

    Add CsvReader.
    Class for reading from csv file.

---
+++ ETLService/Extractors/Readers/CsvReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using ETLService.Models;
+using ETLService.Converters;
+
+namespace ETLService.Extractors.Readers
+{
+    public class CsvReader : FileReader
+    {
+        public CsvReader(string filename, TransactionConverter converter) : base(filename, converter)
+        {
+        }
+
+        public override List<Transaction> Extract()
+        {
+            List<Transaction> transactions = new List<Transaction>();
+            using (StreamReader reader = new StreamReader(_filename))
+            {
+                string line;
+                bool isHeader = true;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (isHeader)
+                        isHeader = false;
+                    else
+                    {
+                        Transaction transaction = _converter.StrToTransaction(line);
+                        transactions.Add(transaction);
+                    }
+                }
+            }
+            return transactions;
+        }
+    }
+}
