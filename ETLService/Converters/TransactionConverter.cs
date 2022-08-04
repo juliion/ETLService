73606c841c56105fc7a248c5e4b30c5b94094590
juliion <juliaionova111@gmail.com>
2022-08-04 18:22:24 +0300

    Replace enum with dictionary in TransactionConverter.

--- ETLService/Converters/TransactionConverter.cs
+++ ETLService/Converters/TransactionConverter.cs
@@ -6,29 +6,33 @@ using ETLService.Models;
 
 namespace ETLService.Converters
 {
-    public enum FieldsIndices
-    {
-        nameInd = 0,
-        surnameInd = 1,
-        cityInd = 2,
-        payentInd = 6,
-        dateInd = 7,
-        accountNumInd = 8,
-        serviceInd = 9
-    }
     public class TransactionConverter
     {
+        private readonly Dictionary<string, int> _fieldsIndices;
+        public TransactionConverter()
+        {
+            _fieldsIndices = new Dictionary<string, int>()
+            {
+                ["name"] = 0,
+                ["surname"] = 1,
+                ["city"] = 2,
+                ["payment"] = 6,
+                ["date"] = 7,
+                ["accountNum"] = 8,
+                ["service"] = 9
+            };
+        }
         public Transaction StrToTransaction(string strTransaction)
         {
             string[] fields = strTransaction.Split(new char[] { ' ', ',', '“', '”' }, StringSplitOptions.RemoveEmptyEntries);
             Transaction transaction = new Transaction()
             {
-                Name = fields[(int)FieldsIndices.nameInd] + " " + fields[(int)FieldsIndices.surnameInd],
-                City = fields[(int)FieldsIndices.cityInd],
-                Payment = decimal.Parse(fields[(int)FieldsIndices.payentInd], CultureInfo.InvariantCulture),
-                Date = DateTime.ParseExact(fields[(int)FieldsIndices.dateInd], "yyyy-mm-dd", CultureInfo.InvariantCulture),
-                AccountNumber = long.Parse(fields[(int)FieldsIndices.accountNumInd]),
-                Service = fields[(int)FieldsIndices.serviceInd]
+                Name = fields[_fieldsIndices["name"]] + " " + fields[_fieldsIndices["surname"]],
+                City = fields[_fieldsIndices["city"]],
+                Payment = decimal.Parse(fields[_fieldsIndices["payment"]], CultureInfo.InvariantCulture),
+                Date = DateTime.Parse(fields[_fieldsIndices["date"]]),
+                AccountNumber = long.Parse(fields[_fieldsIndices["accountNum"]]),
+                Service = fields[_fieldsIndices["service"]]
             };
             return transaction;
         }
